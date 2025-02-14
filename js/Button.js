431bd63a82a96f613f1a15e7aeddef7a218c0759
Abentfork <adrian>
2025-02-14 12:40:56 +0100

    Hecha pagina albumes

--- js/Button.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById('menuButton');
-const desplegable = document.querySelector('.desplegable');
-
-menuButton.addEventListener('click', () => {
-    desplegable.style.display = desplegable.style.display === 'block' ? 'none' : 'block';
-});
